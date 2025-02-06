40ec1d39754d9b7d8c3408b58ee153557b37dfb7
Linda Collins <lindanwajei2205@gmail.com>
2025-02-06 18:32:37 +0000

    add secret

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=w+x&yd!fvc5s61i4$%isu6^*rfh5&(i*@5yx&qi$gq162cs#'
+# SECRET_KEY = 'django-insecure-a=w+x&yd!fvc5s61i4$%isu6^*rfh5&(i*@5yx&qi$gq162cs#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_ENV') == 'development'
