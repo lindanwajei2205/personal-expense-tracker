553f2ec0d13670ea2c06058ee8016737dc635d35
Linda Collins <lindanwajei2205@gmail.com>
2025-02-06 14:20:52 +0000

    add expense category

--- category/apps.py
+++ category/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExpenseConfig(AppConfig):
+class CategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'expense'
+    name = 'category'
