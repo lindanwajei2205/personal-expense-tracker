43584e1701d47d6c792b5dd0ec7a5ac3639c127f
Linda Collins <lindanwajei2205@gmail.com>
2025-02-06 17:33:15 +0000

    Stop tracking env.py

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ['DJANGO_ENV'] = 'development'
-os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:npg_DotZicYbQ13z@ep-muddy-violet-a2wt2vlq.eu-central-1.aws.neon.tech/bride_land_twins_162311'
