6c8b00d3bb496d2cc8a23e681577329080a98791
Linda Collins <lindanwajei2205@gmail.com>
2025-02-09 03:00:40 +0000

    add typography to databoard

--- item/urls.py
+++ item/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('list', views.list_items, name='list_items'),
     path('add', views.add_item, name='add_item'),
-    path('edit/<int:id>', views.edit_item, name='edit_item'),  
+    path('edit/<int:id>', views.edit_item, name='edit_item'),
+    path('delete/<int:id>', views.delete_item, name='delete_item'),
 ]
